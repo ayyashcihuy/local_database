012c6b5f2731e9437a2076abf57eb493552f4c63
ayyashcihuy <ayyashcihuy@gmail.com>
2022-05-30 11:37:08 +0700

    fixing data news

--- database_postdana.id/config/database.js
+++ database_postdana.id/config/database.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  database: "postdana",
-  user: "root",
-  password: "123456",
+  host: "172.31.2.90",
+  database: "neoreksadana",
+  user: "reksaadmin",
+  password: "Bukapintu!0",
 });
 
 module.exports = db;
